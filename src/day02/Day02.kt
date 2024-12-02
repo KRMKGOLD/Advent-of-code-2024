0716d5bb73c61e8684bf6902006bb08e4e8959ed
Seongha Hong <dkqk01242@gmail.com>
2024-12-02 18:25:54 +0900

    refactor duplicate code

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -4,13 +4,8 @@ import println
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.map { it.split(" ").map { it.toInt() } }
-            .count { ints ->
-                val diffs = ints.windowed(2).map { it[1] - it[0] }
-                diffs.all { (1..3).contains(it) } || diffs.all { (-3..-1).contains(it) }
-            }
-    }
+    fun List<String>.toInts(): List<List<Int>> =
+        map { it.split(" ").map { it.toInt() } }
 
     fun List<Int>.omit(index: Int): List<Int> {
         return subList(0, index) + subList(index + 1, size)
@@ -22,8 +17,12 @@ fun main() {
         } || windowed(2).all { it[0] > it[1] && (1..3).contains(it[0] - it[1]) }
     }
 
+    fun part1(input: List<String>): Int {
+        return input.toInts().count { ints -> ints.isValidList() }
+    }
+
     fun part2(input: List<String>): Int {
-        return input.map { it.split(" ").map { it.toInt() } }.count { ints ->
+        return input.toInts().count { ints ->
             ints.isValidList() || ints.indices.any { ints.omit(it).isValidList() }
         }
     }
