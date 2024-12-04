a573fa88663986e216909e4673bb77f8249efb6a
Seongha Hong <dkqk01242@gmail.com>
2024-12-05 00:49:26 +0900

    add day03 part2

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -21,8 +21,53 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
+        val patternRegex = Regex("mul\\(\\d+,\\d+\\)")
+        val enableRegex = Regex("do\\(\\)")
+        val disableRegex = Regex("don't\\(\\)")
+        val numberRegex = Regex("\\d+")
 
-        return input.size
+        var sum = 0
+        var enable = true
+
+        input.forEach {
+            var str = it
+
+            while (true) {
+                val patternStr = patternRegex.find(str)
+                val enableStr = enableRegex.find(str)
+                val disableStr = disableRegex.find(str)
+
+                if (patternStr == null) break
+
+                when (
+                    listOf(
+                        patternStr?.range?.first ?: Int.MAX_VALUE,
+                        enableStr?.range?.first ?: Int.MAX_VALUE,
+                        disableStr?.range?.first ?: Int.MAX_VALUE
+                    ).min()
+                ) {
+                    patternStr?.range?.first -> {
+                        if (enable) {
+                            sum += numberRegex.findAll(patternStr.value).map { it.value.toInt() }
+                                .reduce { acc, i -> acc * i }
+                        }
+                        str = str.substring(patternStr.range.last, str.length)
+                    }
+
+                    enableStr?.range?.first -> {
+                        enable = true
+                        str = str.substring(enableStr.range.last, str.length)
+                    }
+
+                    disableStr?.range?.first -> {
+                        enable = false
+                        str = str.substring(disableStr.range.last, str.length)
+                    }
+                }
+            }
+        }
+
+        return sum
     }
 
     val input = readInput("day03/Day03")
